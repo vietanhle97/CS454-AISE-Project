224fe0341e635c2e5fc041a36577d0b74c13d88d
vietanhle97 <vietanhle0710@gmail.com>
2021-12-05 23:45:23 +0700

    fix error

--- main.py
+++ main.py
@@ -99,30 +99,23 @@ if __name__ == '__main__':
 	sentimentanalysis_options = {
 
 		'embedding_dim': [8, 16, 32, 64, 128, 256, 512, 1024],
-		# please set 1024 to be the maximum value
-		'rnn_hidden_dim': [8, 16, 32, 64, 128, 256, 512, 1024], # can be modified
-		# 
-		# please set 4096 to be the maximum value
-		'rnn_num_layers': [1,2,3,4,5,6,7,8], # can be modified
 
-		# please set 8 to be the maximum value
-		'rnn_dropout': [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85], # can be modified
+		'rnn_hidden_dim': [8, 16, 32, 64, 128, 256, 512, 1024],
 
-		# please set 0.85 to be the maximum value
-		'rnn_bidirectional': [True, False], # can be modified
-		# 
-		'fc_hidden_dim' : [8, 16, 32, 64, 128, 256, 512, 1024], # can be modified
+		'rnn_num_layers': [1,2,3,4,5,6,7,8],
 
-		# please set 4096 to be the maximum value
-		'fc_num_layers' : [1,2,3,4,5,6,7,8], # can be modified
-		# 
-		# please set 8 to be the maximum value
-		'fc_dropout' : [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85], # can be modified
-		# 
-		# please set 0.85 to be the maximum value
-		'learning_rate': [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001], # can be modified
-		# 
-		# please set 0.01 to be the maximum value and 0.00001 to the minimum value
+		'rnn_dropout': [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85],
+
+		'rnn_bidirectional': [True, False], 
+	
+		'fc_hidden_dim' : [8, 16, 32, 64, 128, 256, 512, 1024], 
+
+		'fc_num_layers' : [1,2,3,4,5,6,7,8], 
+		
+		'fc_dropout' : [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85], 
+		
+		'learning_rate': [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001],
+		
 		'batch_size': 32,
 
 		'num_epochs': 10, 
@@ -150,8 +143,8 @@ if __name__ == '__main__':
 
 		"fc_dropout": [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85],
 
-		"learning_rate": [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001], 
-		# ============= do not modify the below hyperparameter
+		"learning_rate": [0.01, 0.005, 0.001, 0.0005, 0.0001, 0.00005, 0.00001],
+		
 		"batch_size": 8, 
 
 		"num_epochs": 10, 
